5527277d57fd1de1e7161c33bfade4afd35a7b67
morjep <94778792+morjep@users.noreply.github.com>
2022-03-26 07:40:39 +0100

    Countdown added

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import handleSubmit from "./js/formhandler";
 import updateList from "./js/countryhandler";
 import generateForecastTable from "./js/generateforecasttable";
+import updateCountdown from "./js/countdown";
 import { serverLog } from "./js/data_util";
 import "./styles/styles.scss";
 
@@ -11,7 +12,10 @@ submitButton.addEventListener("click", (event) => {
   handleSubmit(event); // New name!
 
   // Should check if country is null, i.e. raise alert and exit
+  document.getElementById("forecast").innerHTML = '<table id="forecast-table"></table>';
   generateForecastTable(document.getElementById("forecast-table"));
+
+  updateCountdown();
 
   // addPicture();
 });
